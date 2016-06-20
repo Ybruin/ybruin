7526951fc7b2afc5456534f980d3749484c21f8f
lizijie <leechikit0823@163.com>
2016-06-20 16:16:38 +0800

    功能配置化 0.1.1

--- configs/default.js
+++ configs/default.js
@@ -1,15 +1,26 @@
 module.exports = function(options){
     var projectConf = fis.config.get('projectConf');
-    var domainList = projectConf.domainList;
-    var name = projectConf.name;
-    var terminal = projectConf.terminal;
+    var spriteDefault = {
+        to:'/images/sprites'
+    };
+    var spriteConf = fis.config.get('projectConf.useSprite') || spriteDefault;
+    var domainList = projectConf.domainList || false;
+    var name = projectConf.name || '';
+    var terminal = projectConf.terminal || '';
+    var outputPath = projectConf.outputPath || '../dist';
+    var compressCss = typeof projectConf.compressCss != false ? true : projectConf.compressCss;
+    var compressImg = typeof projectConf.compressImg != false ? true : projectConf.compressImg;
+    var compressJs = typeof projectConf.compressJs != false ? true : projectConf.compressJs;
+    var useSprite = typeof projectConf.useSprite != false ? true : projectConf.useSprite;
+    var useHash = typeof projectConf.useHash != false ? true : projectConf.useHash;
 
 	var plugins = {
 		postpackager:require('../plugins/postpackager/index.js')
 	};
  	fis.hook('commonjs')
 	fis.match('::package', {
-	    postpackager: plugins.postpackager
+	    postpackager: plugins.postpackager,
+        spriter: fis.plugin('csssprites-group')
 	})
 	// 默认产出路径
 	fis.match('/(**)',{
@@ -18,13 +29,22 @@ module.exports = function(options){
     .match('/views/(**)',{
         isViews:true
     })
+    .match('::image', {
+        useHash:useHash
+    })
+    .match('**.{js,css,scss}',{
+        useHash:useHash
+    })
+    .match('**.{css,scss}',{
+        useSprite:useSprite
+    })
     .match('/(js/**)', {
         isMod:true,
         moduleId:'$1',
         id:'$1'
     })
     .match('/sass/(**).scss',{
-        id:'ccs/$1.css',
+        id:'css/$1.css',
         parser: fis.plugin('node-sass'),
         rExt: '.css',
         release:'/'+name+'/'+terminal+'/css/$1'
@@ -32,18 +52,19 @@ module.exports = function(options){
     .match('/sass/(_**.scss)',{
         release:false
     })
-    .match('/components/(**)',{
+    .match('/(components/**)',{
         isMod:true,
         moduleId:'$1',
         id:'$1',
         release:false
     })
+    fis.config.set('settings.spriter.csssprites-group',spriteConf)
 
 	// 发布本地开发版本
-	fis.media('local')
+	fis.media('dev')
     // 启动 fis3-hook-relative 插件
     .hook('relative')
-    .match('*/**', {
+    .match('**', {
         relative:true, // 让所有文件，都使用相对路径
         deploy :fis.plugin('local-deliver',{
             to :"../local"
@@ -51,33 +72,28 @@ module.exports = function(options){
     })
 
 	// 发布线上开发版本：样式压缩，图片合并，png压缩
-	fis.media('online')
+	fis.media('build')
     .match('**.js', {
+        optimizer: compressJs && fis.plugin('uglify-js'),
         domain:domainList
     })
-    // 启用 fis-spriter-csssprites 插件
-    /*.match('::package', {
-        spriter: fis.plugin('csssprites')
-    })*/
     // 对scss进行编译，压缩，图片合并
     .match('**.scss',{
-        optimizer: fis.plugin('clean-css',{
+        optimizer: compressCss && fis.plugin('clean-css',{
             'keepBreaks': true //保持一个规则一个换行
         }),
-        useSprite: true,
         domain:domainList
     })
     // 对css进行压缩，图片合并
     .match('**.css', {
-        optimizer: fis.plugin('clean-css',{
+        optimizer: compressCss && fis.plugin('clean-css',{
             'keepBreaks': true //保持一个规则一个换行
         }),
-        useSprite:true,
         domain:domainList
     })
     // 对图片进行png压缩
     .match('**.png', {
-        optimizer: fis.plugin('png-compressor',{
+        optimizer: compressImg && fis.plugin('png-compressor',{
             'type':'pngquant'
         })
     })
@@ -86,7 +102,7 @@ module.exports = function(options){
     })
     .match('*/**',{
         deploy :fis.plugin('local-deliver',{
-            to :"../online"
+            to :outputPath
         })
     })
 }
